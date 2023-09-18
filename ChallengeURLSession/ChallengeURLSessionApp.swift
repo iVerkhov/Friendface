95268af262e75cc04fc56faf17bbae07f3163093
iVerkhov <vrkhv@iCloud.com>
2023-09-18 19:45:02 +0300

    Initial Commit

---
+++ ChallengeURLSession/ChallengeURLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeURLSessionApp.swift
+//  ChallengeURLSession
+//
+//  Created by Игорь Верхов on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeURLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
